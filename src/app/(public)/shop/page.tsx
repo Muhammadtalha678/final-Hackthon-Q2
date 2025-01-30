ed80b21360266eab071023291f0e0c23be169b81
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-30 16:29:06 +0500

    ADD ERROR FILE

--- src/app/(public)/shop/page.tsx
+++ src/app/(public)/shop/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Shop = async () => {
-  // const response = await fetch() 
+  if (!process.env.base_url) {
+    throw new Error("Base Url is not given");
+    
+  }
+  // const response = await fetch(process.env.base_url) 
   return (
     <div>
       
