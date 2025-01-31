44a1940634c04fdbac615b1a504d4aa5bff8f20c
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-01-31 20:39:28 +0500

    fetch products and create api for categories fetch

--- src/app/(public)/shop/page.tsx
+++ src/app/(public)/shop/page.tsx
@@ -1,3 +1,5 @@
+import ProductListing from '@/components/Product/ProductListing';
+import { Product } from '@/interfaces/Product';
 import React from 'react'
 
 const Shop = async () => {
@@ -5,10 +7,26 @@ const Shop = async () => {
     throw new Error("Base Url is not given!.");
     
   }
-  // const response = await fetch(process.env.base_url) 
+
+  const response = await fetch(`${process.env.base_url}/api/product`)
+  if (!response.ok) {
+    throw new Error("Some thing wen wrong");
+    
+  }
+  // create the responsse interface 
+  const {error,message,data}:{error:boolean,message:string,data:Product[]} = await response.json()
+  
+  console.log(data);
+  
+  
   return (
+
     <div>
-      
+      {
+        data.length > 0 ?
+          <ProductListing products={data}/>  
+          : <h1>Product Not Found</h1>
+      }  
     </div>
   )
 }
