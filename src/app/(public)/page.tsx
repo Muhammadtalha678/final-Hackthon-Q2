3a00d0086866a3bfbf369e984df14b2813851b61
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-06 00:27:21 +0500

    ..

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -6,9 +6,9 @@ import OurProducts from "@/components/home/OurProducts";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-  const user =await currentUser()
-  console.log(user?.id);
-  
+  const user = await currentUser()
+  // console.log(user?.id);
+
   return (
     // <div className="flex justify-center items-center">
     //   <h1 className="text-4xl text-green-950">{user ? 'Dashboard' : 'Login'}</h1>
@@ -16,7 +16,7 @@ export default async function Home() {
     <div>
       <Hero />
       <Category />
-      <OurProducts/>
+      <OurProducts />
     </div>
   );
 }
