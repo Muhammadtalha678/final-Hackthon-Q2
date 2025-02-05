76d5cbab832d6f7a0905bd0edb848d516030b75b
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-06 00:24:19 +0500

    add protected routes & redirect user

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,32 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { redirect } from "next/dist/server/api-utils";
+import { NextResponse } from "next/server";
 
-const isProtectedRoutes = createRouteMatcher(['/shipment'])
+const isProtectedRoutes = createRouteMatcher(['/shipment', '/cart', '/checkout'])
 
 export default clerkMiddleware(async (auth, req) => {
-    console.log(req.url);
-
+    const url = new URL(req.url)
     if (isProtectedRoutes(req)) await auth.protect()
+
+    if (url.pathname === '/checkout') {
+        console.log(url.pathname);
+        //get previous route where it come from 
+        const referrer = req.headers.get('referer')
+        // console.log(!referrer); if user come from unkonu=wn url
+        console.log(!referrer?.includes('/cart')); //if previous route not contain '/cart' then also redirect
+        // to cart
+
+        if (!referrer || !referrer.includes('/cart')) {
+            // console.log(!referrer?.includes('/cart'));
+            return Response.redirect(new URL('/cart', req.url))
+        }
+
+    }
+    // Proceed with the request if no redirection is needed
+    return NextResponse.next()
 });
+
+
 
 export const config = {
     matcher: [
