a186d39b5e309fc1cf3d655a820977f14f19aeb3
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-05 18:07:35 +0500

    remove links to pas as prop

--- src/components/resuable/Hero2.tsx
+++ src/components/resuable/Hero2.tsx
@@ -1,27 +1,33 @@
-
+'use client'
 import Image from "next/image";
 import Link from "next/link";
-
+import { usePathname } from 'next/navigation'
 interface Hero2Props {
   title: string;
   backgroundImage: string;
-  links: { name: string; href: string }[];
+  // links: { name: string; href: string }[];
 }
 
-export const Hero2: React.FC<Hero2Props> = ({ title, backgroundImage, links }) => {
+export const Hero2: React.FC<Hero2Props> = ({ title, backgroundImage }) => {
+  const pathname = usePathname()
+
+  const links = [
+    { name: "Home", href: '/' },
+    { name: pathname.charAt(1).toUpperCase() + pathname.slice(2, pathname.length), href: pathname },
+  ]
   return (
     <div className="relative w-full h-56 flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className="absolute inset-0 bg-black bg-opacity-30"></div> {/* Overlay */}
       <div className="relative z-10 text-center">
-      <Image src='/images/mainLogo.png' width={100} height={100} alt=""/>
+        <Image src='/images/mainLogo.png' width={100} height={100} alt="" />
         <h1 className="text-3xl font-bold text-black">{title}</h1>
         <nav className="mt-2 text-sm text-gray-600">
           {links.map((link, index) => (
             <span key={index}>
-              <Link href={link.href} className="font-medium text-black hover:text-gray-700">
+              <Link href={link.href} className="font-medium  text-white hover:text-gray-700">
                 {link.name}
               </Link>
-              {index < links.length - 1 && <span className="mx-2">›</span>}
+              {index < links.length - 1 && <span className="mx-2 text-2xl font-medium text-white">›</span>}
             </span>
           ))}
         </nav>
@@ -30,4 +36,3 @@ export const Hero2: React.FC<Hero2Props> = ({ title, backgroundImage, links }) =
   );
 };
 
-   
