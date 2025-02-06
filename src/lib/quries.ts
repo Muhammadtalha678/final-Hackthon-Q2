c11766801156083ecc084ff7ad49911f2da6678c
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-06 19:27:48 +0500

    add fetch single product

--- src/lib/quries.ts
+++ src/lib/quries.ts
@@ -1,7 +1,7 @@
 //finds all product
 export const ALL_PRODUCT_QUERY = `*[_type == "products"]`
 //finds product by Producy id
-export const SINGLE_PRODUCTS = `*[_type == "products" && _id == $id][0]`
+export const SINGLE_PRODUCT = `*[_type == "products" && _id == $id][0]`
 
 //finds all category
 export const ALL_CATEGORY_QUERY = `*[_type == "categories"]`
