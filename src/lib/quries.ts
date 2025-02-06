f85a857538e7d3700f529970eea2bd7f0492f1a7
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-06 17:16:02 +0500

    create api for fetching products by category

--- src/lib/quries.ts
+++ src/lib/quries.ts
@@ -2,3 +2,5 @@
 export const ALL_PRODUCT_QUERY = `*[_type == "products"]`
 //finds all category
 export const ALL_CATEGORY_QUERY = `*[_type == "categories"]`
+//finds products by caegory id
+export const CATEGORY_PRODUCTS = `*[_type == "products" && category._ref == $id]`
