03a60fde614e405a62619f52afca337de8a2d410
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-06 14:28:40 +0500

    create the root layout and call all providers

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import { CartContextProvider } from '@/context/CartContext';
+import { WishListProvider } from '@/context/WishListContext';
+import { poppins } from '@/lib/fonts';
+import { ClerkProvider } from '@clerk/nextjs';
+import React from 'react'
+
+const RootLayout = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    return (
+        <html lang="en">
+            <body className={`${poppins.className}`}>
+                <ClerkProvider>
+                    <CartContextProvider>
+                        <WishListProvider>
+
+                            <div className="max-w-[1440px] mx-auto">
+                                {children}
+
+                            </div>
+                        </WishListProvider>
+
+                    </CartContextProvider>
+
+                </ClerkProvider>
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout
