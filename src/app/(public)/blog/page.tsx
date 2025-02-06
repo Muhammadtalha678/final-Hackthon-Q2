28f5614cec21fa4f3d58505ff288a343833fcaca
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-07 02:14:46 +0500

    add the blog page

--- src/app/(public)/blog/page.tsx
+++ src/app/(public)/blog/page.tsx
@@ -1,5 +1,7 @@
+import { Hero2 } from '@/components/resuable/Hero2';
 import { Metadata } from 'next';
 import React from 'react'
+import BlogPage from './blogPage';
 
 export const metadata: Metadata = {
   title: "Blog | Furniro",
@@ -9,6 +11,8 @@ export const metadata: Metadata = {
 const Blog = () => {
   return (
     <div>
+      <Hero2 title='Blog' backgroundImage='/images/hero2.jpg' />
+      <BlogPage />
     </div>
   )
 }
