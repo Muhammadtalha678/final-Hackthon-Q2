ef21a153bd25fb472708b90ee1c50848e3dfafee
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-06 18:50:52 +0500

    remove div

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,10 +15,7 @@ const RootLayout = ({
                 <ClerkProvider>
                     <CartContextProvider>
                         <WishListProvider>
-
-                            <div className="max-w-[1440px] mx-auto">
-                                {children}
-                            </div>
+                            {children}
                         </WishListProvider>
 
                     </CartContextProvider>
