fdb2f64fd6ba4e95be1cc8597ab2ad054ddce7e2
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-06 14:33:05 +0500

    add metadata for each protected route

--- src/app/(protected)/cart/page.tsx
+++ src/app/(protected)/cart/page.tsx
@@ -1,6 +1,12 @@
 import CartTable from '@/components/cart/cartTable/cartTable'
 import Services from '@/components/resuable/Services'
+import { Metadata } from 'next';
 import React from 'react'
+
+export const metadata: Metadata = {
+  title: "Your Cart | Furniro",
+  description: "View and manage your selected furniture items before checkout.",
+};
 
 const Cart = () => {
   return (
--- src/app/(protected)/checkout/page.tsx
+++ src/app/(protected)/checkout/page.tsx
@@ -1,9 +1,13 @@
+import { Metadata } from 'next';
 import React from 'react'
 
+export const metadata: Metadata = {
+  title: "Checkout | Furniro",
+};
 const Checkout = () => {
   return (
     <div>
-      
+
     </div>
   )
 }
