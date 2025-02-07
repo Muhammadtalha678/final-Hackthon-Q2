3deea0f575c3d6188b481489c11605b9fa091e5d
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-07 22:49:57 +0500

    add unauthorized page

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -1,7 +1,15 @@
-export default function Unauthorized() {
+
+import { Button } from "@/components/ui/button";
+import { headers } from 'next/headers'
+import Link from "next/link";
+export default async function Unauthorized() {
+
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col gap-3 justify-center items-center h-screen">
             <h1 className="text-red-600 text-3xl font-bold">Access Denied</h1>
+            <Link href={`/`}>
+                <Button variant={'btnSecondary'}>Go Back</Button>
+            </Link>
         </div>
     );
 }
