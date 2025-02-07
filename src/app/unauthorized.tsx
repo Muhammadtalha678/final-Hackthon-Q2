3c3a6e53d38cc790cf39a1e888c1df0d8d92bf47
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-07 18:55:19 +0500

    remove file

--- src/app/unauthorized.tsx
+++
@@ -1,7 +0,0 @@
-export default function Unauthorized() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <h1 className="text-red-600 text-3xl font-bold">Access Denied</h1>
-        </div>
-    );
-}
