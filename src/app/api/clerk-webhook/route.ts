bd34cd3a4cab0f2ce35aa6b6c3873de953bd6e11
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-10 00:40:42 +0500

    add webhook clerk api

--- src/app/api/clerk-webhook/route.ts
+++ src/app/api/clerk-webhook/route.ts
@@ -1,29 +1,61 @@
-import { client } from "@/sanity/lib/client";
-import { NextRequest, NextResponse } from "next/server";
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { WebhookEvent } from '@clerk/nextjs/server'
+import { client } from '@/sanity/lib/client'
 
-export async function POST(req: NextRequest) {
-    try {
-        const { id, firstName, lastName, email } = await req.json();
-        if (!email || !id) {
-            return NextResponse.json({ error: true, message: "Invalid Data", data: null })
+export async function POST(req: Request) {
+    const SIGNING_SECRET = process.env.SIGNING_SECRET
 
-        }
-        const checkUserExists = await client.fetch(`*[_type == "user" && email == $email][0]`, { email })
-        if (checkUserExists) {
-            return NextResponse.json({ error: true, message: "User Already Exists", data: null })
-
-        }
-
-        const user = await client.create({
-            _type: 'user',
-            userId: id,
-            name: `${firstName} ${lastName}`,
-            email: email,
-            role: "user",
-        })
-        return NextResponse.json({ error: true, message: "User created in Sanity", data: user })
-    } catch (error) {
-
-        return NextResponse.json({ data: "Error" + error })
+    if (!SIGNING_SECRET) {
+        throw new Error('Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local')
     }
+
+    // Create new Svix instance with secret
+    const wh = new Webhook(SIGNING_SECRET)
+
+    // Get headers
+    const headerPayload = await headers()
+    const svix_id = headerPayload.get('svix-id')
+    const svix_timestamp = headerPayload.get('svix-timestamp')
+    const svix_signature = headerPayload.get('svix-signature')
+
+    // If there are no headers, error out
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+        return new Response('Error: Missing Svix headers', {
+            status: 400,
+        })
+    }
+
+    // Get body
+    const payload = await req.json()
+    const body = JSON.stringify(payload)
+
+    let evt: WebhookEvent
+
+    // Verify payload with headers
+    try {
+        evt = wh.verify(body, {
+            'svix-id': svix_id,
+            'svix-timestamp': svix_timestamp,
+            'svix-signature': svix_signature,
+        }) as WebhookEvent
+    } catch (err) {
+        console.error('Error: Could not verify webhook:', err)
+        return new Response('Error: Verification error', {
+            status: 400,
+        })
+    }
+
+    // Do something with payload
+    // For this guide, log payload to console
+    const { id } = evt.data
+    const eventType = evt.type
+    if (eventType === 'user.created') {
+        const sanityUser = await client.fetch(`*[_type == 'user' && userId == $id][0]`)
+        if (sanityUser) return Response.json({ error: true, message: "User already exists" })
+    }
+    console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
+    console.log('Webhook payload:', body)
+
+    return new Response('Webhook received', { status: 200 })
 }
