1eec47d06933a7f14e8c3f2ffb6aca211de80fd3
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-12 13:15:24 +0500

    add server action for submittin order

---
+++ src/app/actions/actions.ts
@@ -0,0 +1,8 @@
+"use server";
+
+import { CheckOutValidationSchema } from "@/utils/validations/checkoutValidation";
+
+export async function checkoutAction(prevState: any, formData: CheckOutValidationSchema) {
+
+    
+}
