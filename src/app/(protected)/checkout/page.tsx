ca51852797ee899a1019696441a6456738d4468f
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-12 16:32:55 +0500

    remove eslin error

--- src/app/(protected)/checkout/page.tsx
+++ src/app/(protected)/checkout/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from 'next';
 import React from 'react'
-import CheckoutSummary from './CheckoutSubTotal';
 import BillingDetails from './BillingDetails';
 
 export const metadata: Metadata = {
