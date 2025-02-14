dafb0b6426a887d1390e7585989f4630f1e68e42
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-02-14 09:01:16 +0500

    add sale table in index

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import { type SchemaTypeDefinition } from 'sanity'
 import category from './category'
 import product from './product'
 import user from './user'
+import sale from './sale'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [category, product, user],
+  types: [category, product, user, sale],
 }
