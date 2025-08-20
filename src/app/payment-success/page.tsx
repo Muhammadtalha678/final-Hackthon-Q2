01bfbf8ca3fa1e6249e49b491d077b335fcddaf5
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-08-20 20:21:20 +0500

    .

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -2,11 +2,14 @@
 import React from 'react'
 import SuccessDialog from "../../components/SuccessDialog"
 import { redirect } from 'next/navigation';
+import { SESSION_ID } from 'sanity';
 interface Props {
     searchParams: { session_id?: string };
 }
 const PaymentSuccess = async ({ searchParams }: { searchParams: Promise<Props> }) => {
     const { session_id } = (await searchParams).searchParams
+    console.log(session_id);
+
 
     if (!session_id) {
         redirect('/cart')
