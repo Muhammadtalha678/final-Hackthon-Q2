c13259f5c32f6f57559725de738051345a1ee20f
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-08-20 18:04:40 +0500

    create the action just for test

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,10 +1,68 @@
 "use server";
 
+import { Product } from "@/interfaces/Product";
+import { client } from "@/sanity/lib/client";
 // import { client } from "@/sanity/lib/client";
 import { CheckOutValidationSchema } from "@/utils/validations/checkoutValidation";
+import { auth } from "@clerk/nextjs/server";
+export async function checkoutAction(prevState: unknown, formData: CheckOutValidationSchema) {
+    const user = (await auth()).userId
+    if (!user) {
+        return {
+            error: true, message: 'User not authenticate'
+        }
+    }
+    try {
+        await Promise.all(
+            [
+                client.create({
+                    _type: 'sales',
+                    customerId: user,
+                    deliveryAddress: {
+                        name: `${formData.firstname} ${formData.lastname}`,
+                        address: formData.address,
+                        city: formData.town,
+                        country: formData.country,
+                        zipcode: formData.zipcode
+                    },
+                    paymentStatus: 'Cash on delivery',
+                    deliveryStatus: "pending",
+                    sales_price: formData.sales_price,
+                    product_detail: formData.product_detail
 
-export async function checkoutAction(formData: CheckOutValidationSchema) {
-    console.log(formData);
+                })
+                ,
+                // update inventory
+                formData.product_detail.map(async (e) => {
+                    const productData = await client.fetch(`*[_type == 'products' && _id == $id][0]`, { id: e.productId })
+                    const stockUpdate = productData.stock - e.quantity_sold
+
+                    await client.patch(e.productId)
+                        .set({ stock: stockUpdate })
+                        .commit()
+
+                }),
+            ]
+        )
+
+        return {
+            message: "Order Placed Successfully",
+            error: false,
+        }
+
+    } catch (error) {
+        const err = error as Error
+        return {
+            message: err.message,
+            error: true,
+        }
+
+    }
+
+
+    // client.delete({
+    //     query: `*[_type == "sales"]`
+    // })
 
 
 
