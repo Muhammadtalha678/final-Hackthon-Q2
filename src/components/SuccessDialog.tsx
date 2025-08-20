26761dbf427d845b54ca360029e9f5ee2fe8d5e7
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-08-20 21:45:44 +0500

    .

--- src/components/SuccessDialog.tsx
+++ src/components/SuccessDialog.tsx
@@ -6,9 +6,8 @@ import { useCart } from '@/context/CartContext'
 import { useEffect } from "react";
 export default function PaymentSuccessDialog() {
     const { emptyCart } = useCart()
-    useEffect(() => {
-        emptyCart()
-    }, []);
+    emptyCart()
+
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
             <div className="bg-white rounded-lg shadow-lg max-w-md w-full p-6">
