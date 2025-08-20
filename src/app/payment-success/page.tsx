5296d7dd9ac91b3f97095d0628275efb4d222347
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-08-20 18:03:53 +0500

    add the payent success page  as a  success  stripe url

---
+++ src/app/payment-success/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SuccessDialog from "../../components/SuccessDialog"
+
+const PaymentSuccess = () => {
+    return (
+        <SuccessDialog />
+    )
+}
+
+export default PaymentSuccess
