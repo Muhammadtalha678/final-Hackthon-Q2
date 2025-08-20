f3421b73a424bc7f4ba902e9d32e2ca752f4b905
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-08-20 20:04:00 +0500

    server side validation

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -1,27 +1,16 @@
 'use client'
 import React from 'react'
 import SuccessDialog from "../../components/SuccessDialog"
-import { useCart } from '@/context/CartContext'
-import { useEffect } from "react";
-import { useSearchParams, useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation';
+interface Props {
+    searchParams: { session_id?: string };
+}
+const PaymentSuccess = ({ searchParams }: Props) => {
+    const sessionId = searchParams.session_id
 
-const PaymentSuccess = () => {
-    const { emptyCart } = useCart()
-    const searchParams = useSearchParams()
-    const router = useRouter()
-
-    useEffect(() => {
-
-        const sessionId = searchParams.get("session_id")
-
-        if (!sessionId) {
-            // agar session_id nahi hai to cart page bhej do
-            router.replace("/cart")
-        }
-        emptyCart();
-        console.log("Cart after clear:", localStorage.getItem("cart"));
-    }, [searchParams, router]);
-
+    if (!sessionId) {
+        redirect('/cart')
+    }
     return (
         <SuccessDialog />
     )
